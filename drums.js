5a27b4a3bb1c5516d17cbca82539329ffd101cc9
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-02-18 10:45:05 +0530

    initial commit

--- drums.js
+++ drums.js
@@ -1,7 +1,45 @@
 function onClick(){
-    const audio  = new Audio("Assets/sounds/crash.mp3");
+  changeAudio();
+}
+function changeAudio() {
+    const btnInner = this.innerHTML; 
+
+    switch(btnInner){
+    case 'w' :
+    var audio  = new Audio("Assets/sounds/crash.mp3");
     audio.play();
-    console.log(this);
+    break;
+    
+    case 'a' :
+    var audio  = new Audio("Assets/sounds/tom-1.mp3");
+    audio.play();
+    break;
+
+    case 's':
+    var audio = new Audio("Assets/sounds/snare.mp3");
+    audio.play();
+    break;
+
+    case 'd':
+    var audio = new Audio("Assets/sounds/tom-3.mp3");
+    audio.play();
+    break;
+
+    case 'j':
+    var audio = new Audio("Assets/sounds/tom-4.mp3");
+    audio.play();
+    break;
+
+    case 'k':
+    var audio = new Audio("Assets/sounds/tom-2.mp3");
+    audio.play();
+    break;
+
+    case 'l':
+    var kick = new Audio("Assets/sounds/Aadat.mp3");
+    kick.play();
+    break;
+    }
 }
 
 // if we want ot select all queryy with same name we must use a foreacch loop for traversing all of them and we must use className with .
