f192eccabc71bdbefa792987bb77d8546b813bc1
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-02-20 10:59:43 +0530

    initial commit

--- Assets/js/first.js
+++ Assets/js/first.js
@@ -1,10 +1,16 @@
-console.log("hello from Node");
+// console.log("hello from Node");
 
 //A call back Method
 
 const fs = require("fs");
+// const fs1 = require("");
 
-fs.writeFile("Max.txt", "Hello Form Node.JS", (err) => {
-    if(err) throw err;
-    console.log("File is Saved");
-});
+// fs.writeFile("Max.txt", "Hello Form Node.JS", (err) => {
+//     if(err) throw err;
+//     console.log("File is Saved");
+// });
+
+fs.readFile("Max.txt", 'utf8', (err, data) => {
+   if(err) throw err;
+   console.log(data);
+})
