837254a3bacda648aeac0a6599646594db8a979a
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-02-20 19:33:21 +0530

    initial commit

---
+++ NPM/qrCheck.js
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+        "message" : "Enter Your Url",
+         "name" : "URL"
+    }
+  ])
+  .then((answers) => {
+     const url = answers.URL;
+     
+     var qr_svg = qr.image(url);
+     qr_svg.pipe(fs.createWriteStream('max.png'));
+
+     fs.writeFile('Mat.txt', url, (err) => {
+     if (err) throw err;
+     console.log('The file has been saved!');
+
+}); 
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
