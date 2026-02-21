7c07bece1ab8879abde27a3a6f263f5e38f54c54
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-02-21 16:13:10 +0530

    initial commit

--- Express_Server/express1.js
+++ Express_Server/express1.js
@@ -2,6 +2,19 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.send("<h1>Express js</h1>");
+    // console.log(req.rawHeaders);
+});
+app.get("/about", (req, res) => {
+    res.send("<h2>About page</h2>");
+    // console.log(req.rawHeaders);
+});
+app.get("/contact", (req, res) => {
+    res.send("<h2>Contact page</h2>");
+    // console.log(req.rawHeaders);
+});
+
 app.listen(port, () => {
     console.log(`Server is Running on Port ${port}.`);
 })
