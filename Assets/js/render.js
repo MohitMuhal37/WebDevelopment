381db4dccd5aa1c8dd2d7a143f87b01a93a5c0f0
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-03-14 14:04:12 +0530

    initail commit

--- Assets/js/render.js
+++ Assets/js/render.js
@@ -1,7 +1,16 @@
 import express from "express";
 const app = express();
+const port = 3000;
+app.listen(port,() => {
+    console.log(`${port}`);
+    
+});
 
-app.post("/submit",(req, res) => {
+app.get('/',(req, res) => {
+    res.send("h1");
+})
+
+app.post("/",(req, res) => {
     const name = req.body['name'];
     res.send(`<h1>Hello ${name}</h1>`);
 });
