f523926e4bc4011106169043a3a9c040a03c6006
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-03-23 19:18:58 +0530

    initial commit

--- React/my-app/src/index.jsx
+++ React/my-app/src/index.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDom from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<h1>Hello React</h1>);
-
-export default index
+const name = 'Mohit';
+const root = ReactDom.createRoot(document.getElementById('root'));
+const num = 5;
+root.render(<>
+    <h1>Hi I am Heading</h1>
+    <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+    </ul>
+    <p>Hi {name} Your Lucky Number is {Math.floor(Math.random()*10+1)}</p>
+</>
+);
