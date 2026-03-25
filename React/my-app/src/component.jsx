364653851f785c2c569e28eedd2986154602bdbc
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-03-25 18:10:13 +0530

    initial commit

--- React/my-app/src/component.jsx
+++ React/my-app/src/component.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function Heading(){
-    return <h1>Hi i am New heading</h1>
+    return <> 
+    <h1>Hi i am New heading</h1>
+    <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+    </ul>
+    </>
 };
 
-export default Heading;
+function List(){
+    return <>
+    <ul>
+        <li>Rajma</li>
+        <li>Chawal</li>
+        <li>Bhindi</li>
+    </ul>
+    </>
+}
+export default Heading; List;
