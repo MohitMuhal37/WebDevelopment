dc3b5bf591c9cbde9a0de137b7e6e503aa5d1405
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-03-25 19:08:33 +0530

    initial commit

--- React/my-app/src/new.jsx
+++ React/my-app/src/new.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import Time from "./Time";
 import Pi, {double}from "./math";
+import * as calc from "./Calculate";
 
 const root =  ReactDom.createRoot(document.getElementById('root'));
 
 root.render(<>
 <h1>Hi</h1>
-<li>{Pi}</li>
-<li>{double()}</li>
+<li>{calc.default()}</li>
+<li>{calc.add(45,5)}</li>
+<li>{calc.mul(5, 6)}</li>
+<li>{calc.sub(5, 2)}</li>
 <Time />
 </>)
