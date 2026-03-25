f10d7014c57955b3bf2199a4c7a7cf897ddbeba6
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-03-25 18:05:18 +0530

    initial commit

---
+++ React/my-app/src/component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Heading(){
+    return <h1>Hi i am New heading</h1>
+};
+
+export default Heading;
--- React/my-app/src/practice.jsx
+++ React/my-app/src/practice.jsx
@@ -1,19 +1,31 @@
 import "../src/style.css";
 import React from "react";
 import ReactDom from "react-dom/client";
-const name = 'Mohit';
-const year = 2026
+import Heading from "./component.jsx"
+// const name = 'Mohit';
+// const year = 2026
 
 const root = ReactDom.createRoot(document.getElementById('root'));
 
 const img = 'https://picsum.photos/200';
 
-{color:"brown"}
+// {color:"brown"}
+// root.render(<>
+// <h1  style={{color:"brown"}}>Hi i am heading</h1>
+//     <ul>
+//         <img src="../public/favicon.svg" alt="ko" />
+//         <img src={img+ '?grayscale'} alt="" />
+//     </ul>
+// </>
+// );
+
+// function Head(){
+//     return <h1>Hi i am Heading</h1>
+// }
+
+// root.render(<>
+//     <Head />
+// </>)
 root.render(<>
-<h1  style={{color:"brown"}}>Hi i am heading</h1>
-    <ul>
-        <img src="../public/favicon.svg" alt="ko" />
-        <img src={img+ '?grayscale'} alt="" />
-    </ul>
-</>
-);
+<Heading />
+</>)
