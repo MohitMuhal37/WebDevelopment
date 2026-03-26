cf5ec6bdb0bdaefb2edd5d12ee669ef15aa537a4
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-03-26 14:41:53 +0530

    initial commit

--- React/my-app/src/Calculate.jsx
+++ React/my-app/src/Calculate.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 function Calculate(){
     return <h1>Calculation file</h1>
-}
+};
 
 function add(a, b){
     return a+b;
-}
+};
 
 function sub(a, b){
     return a-b;
-}
+};
 
 function mul(a, b){
     return a*b;
-}
+};
+
 export default Calculate;
 export {sub, mul, add}
